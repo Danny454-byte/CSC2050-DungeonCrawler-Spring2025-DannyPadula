10d66a44485daf6886bca90aa30864091e93b47d
Danny454-byte <daniel.padula@student.cuaa.edu>
2025-02-27 15:03:51 -0500

    Initial commit

---
+++ Objects/Exit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Exit 
+{
+private string direction;
+Room destination; 
+public Exit(string direction, Room destination)
+{
+this.direction = direction;
+this.destination = destination; 
+}
+}
